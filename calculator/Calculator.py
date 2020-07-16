05c309053c098b275c4bc310ecded081d4fbb6f4
sevda <imanysevda@gmail.com>
2020-07-16 16:06:46 +0430

    Add frames

--- calculator/Calculator.py
+++ calculator/Calculator.py
@@ -7,4 +7,17 @@ root.resizable(width = False , height = False)
 root.title('calculator')
 color = 'pink'
 root.configure(bg = color)
+#============================================================= frames ================================================================
+top_first = Frame(root, width = 400 ,height = 50 , bg = color)
+top_first.pack(side = TOP)
+
+top_second = Frame(root, width = 400 ,height = 50 , bg = color)
+top_second.pack(side = TOP)
+
+top_third = Frame(root, width = 400 ,height = 50 , bg = color)
+top_third.pack(side = TOP)
+
+top_forth = Frame(root, width = 400 ,height = 50 , bg = color)
+top_forth.pack(side = TOP)
+
 root.mainloop()
