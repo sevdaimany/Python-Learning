e870d76927e7727a56b39d4674c1622017c367de
sevda <imanysevda@gmail.com>
2020-07-16 16:26:33 +0430

    Add buttons

--- calculator/Calculator.py
+++ calculator/Calculator.py
@@ -20,4 +20,16 @@ top_third.pack(side = TOP)
 top_forth = Frame(root, width = 400 ,height = 50 , bg = color)
 top_forth.pack(side = TOP)
 
+#========================================================== buttons ============================================================================
+btn_plus = Button(top_third , text = '+' , width = 10 , bg = 'light blue')
+btn_plus.pack(side= LEFT , padx = 5 ,pady = 5)
+
+btn_minus = Button(top_third , text = '-' , width = 10 , bg = 'light blue')
+btn_minus.pack(side= LEFT , padx = 5 ,pady = 5)
+
+btn_mul = Button(top_third , text = '*' , width = 10 , bg = 'light blue')
+btn_mul.pack(side= LEFT , padx = 5 ,pady = 5)
+
+btn_div = Button(top_third , text = '/' , width = 10 , bg = 'light blue')
+btn_div.pack(side= LEFT , padx = 5 ,pady = 5)
 root.mainloop()
