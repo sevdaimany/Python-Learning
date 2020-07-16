8110ac06acc8ed9df32b219c6760b540427815f5
sevda <imanysevda@gmail.com>
2020-07-16 17:36:08 +0430

    Add operation function and variables

--- calculator/Calculator.py
+++ calculator/Calculator.py
@@ -20,39 +20,63 @@ top_third.pack(side = TOP)
 top_forth = Frame(root, width = 400 ,height = 50 , bg = color)
 top_forth.pack(side = TOP)
 
+#=================================================== variabels ======================================================================================
+num1 = StringVar()
+num2 =StringVar()
+answer =StringVar()
+
+
 #========================================================== buttons ============================================================================
-btn_plus = Button(top_third , text = '+' , width = 10 , bg = 'paleturquoise')
+btn_plus = Button(top_third , text = '+' , width = 10 , bg = 'paleturquoise', command = lambda: operation('+'))
 btn_plus.pack(side= LEFT , padx = 5 ,pady = 5)
 
-btn_minus = Button(top_third , text = '-' , width = 10 , bg = 'paleturquoise')
+btn_minus = Button(top_third , text = '-' , width = 10 , bg = 'paleturquoise', command = lambda: operation('-'))
 btn_minus.pack(side= LEFT , padx = 5 ,pady = 5)
 
-btn_mul = Button(top_third , text = '*' , width = 10 , bg = 'paleturquoise')
+btn_mul = Button(top_third , text = '*' , width = 10 , bg = 'paleturquoise',  command = lambda: operation('*'))
 btn_mul.pack(side= LEFT , padx = 5 ,pady = 5)
 
-btn_div = Button(top_third , text = '/' , width = 10 , bg = 'paleturquoise')
+btn_div = Button(top_third , text = '/' , width = 10 , bg = 'paleturquoise', command = lambda: operation('/'))
 btn_div.pack(side= LEFT , padx = 5 ,pady = 5)
 
 #========================================================== Entries and Labels ========================================================================
 label_first_num = Label(top_first, text = 'Input Number1:' , bg  ='lightgray')
 label_first_num.pack(side=LEFT, pady=10, padx=10)
 
-first_num = Entry(top_first)
+first_num = Entry(top_first,textvariable=num1)
 first_num.pack(side = LEFT)
 
 label_second_num = Label(top_second, text = 'Input Number2:' , bg  ='lightgray')
 label_second_num.pack(side=LEFT, pady=10, padx=10)
 
-second_num = Entry(top_second)
+second_num = Entry(top_second,textvariable=num2)
 second_num.pack(side=LEFT)
 
 
 result = Label(top_forth, text = 'Result: ' , bg  ='lightgray',  width = 10)
 result.pack(side=LEFT, pady=10, padx=10)
 
-res_num =Label(top_forth, bg  ='white'  , width = 10)
+res_num =Label(top_forth, bg  ='white'  , width = 10 , textvariable = answer)
 res_num.pack(side=LEFT)
 
 
+
+# ================================================== functions =======================================================================================
+
+def operation(inputOperation):
+    
+    if inputOperation == '+':
+       answer.set(float(num1.get()) + float(num2.get()))
+
+    elif inputOperation == '-':
+        answer.set(float(num1.get()) - float(num2.get()))
+
+    elif inputOperation == '*':
+       answer.set(float(num1.get())  * float(num2.get()))
+           
+
+    elif inputOperation == '/':
+       answer.set(float(num1.get()) / float(num2.get()))
+           
 
 root.mainloop()
