2bcf3c1482fc3f8f41d073ecd840f0f8d9ca85f6
sevda <imanysevda@gmail.com>
2020-07-16 15:57:17 +0430

    Add setting

---
+++ calculator/Calculator.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+#============================================================== settings ===========================================================
+root = Tk()
+root.geometry('400x200')
+root.resizable(width = False , height = False)
+root.title('calculator')
+color = 'pink'
+root.configure(bg = color)
+root.mainloop()
